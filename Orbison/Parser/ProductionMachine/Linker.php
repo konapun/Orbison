5ec941a9331ee07c09adb0232d73a10e5b745a78
Bremen Braun <bremen.braun@gmail.com>
2017-03-31 23:08:33 -0500

    Linker cleanup

--- Orbison/Parser/ProductionMachine/Linker.php
+++ Orbison/Parser/ProductionMachine/Linker.php
@@ -3,31 +3,20 @@ namespace Orbison\Parser\ProductionMachine;
 
 use Orbison\Parser\PDA as PDA;
 
-class Placeholder {
-  private $incoming;
-  private $outgoing;
-
-  function __construct($incoming=array(), $outgoing=array()) {
-
-  }
-}
-
 /*
- * <phrase> ::= "hello" "," <target> <punctuation> "!"
- *              | "done";
+ * <phrase> ::= "hello" "," <target> <punctuation> <phrase>
+ *            | "done" "!";
  * <target> ::= "world"
  *            | [STRING];
  * <punctuation> ::= "!"
- *                 | "?"
- *                 | ".";
+ *                 | "."
+ *                 | "?";
  */
 class Linker {
   private $pda;
-  private $linkedProductions;
 
   function __construct() {
     $this->pda = new PDA();
-    $this->linkedProductions = array();
   }
 
   function link($production) {
@@ -35,91 +24,66 @@ class Linker {
 
     $acceptNodes = $this->linkProduction($production, PDA::START);
     foreach ($acceptNodes as $acceptNode) {
-      print "Adding transition from $acceptNode on ACCEPT to ACCEPT\n";
+      print "Adding ACCEPT transition from $acceptNode\n";
       $pda->addTransition($acceptNode, PDA::ACCEPT, PDA::ACCEPT);
+      // $acceptNode->addTransition(PDA::ACCEPT, PDA::ACCEPT); // FIXME - change once new PDA impelmented
     }
 
     return $pda;
   }
 
-  private function linkProduction($production, $incomingNodes, $factorIndex=0) {
-    print "+-------------------------------------------------+\n";
-    print "| Linking production " . $production->getID() . " |\n";
-    print "+-------------------------------------------------+\n";
-    if (!is_array($incomingNodes)) $incomingNodes = array($incomingNodes);
+  function linkProduction($production, $incoming, $recursingOnTerm=null) {
+    if (!is_array($incoming)) $incoming = array($incoming);
 
-    $outgoingNodes = array();
-    if ($this->haveSeenProduction($production, $factorIndex)) {
-      return array();
-      print "*** On SEEN production " . $production->getID() . " with incoming nodes " . $this->PRINT_ARRAY($incomingNodes) . "\n";
-      return $incomingNodes; // FIXME
+    $outgoing = array();
+    if ($recursingOnTerm) { // happens when a production reference is within the reference itself in a terminal position
+      print "RECURSING! Production " . $production->getID() . " Term " . $recursingOnTerm->getID() . "!!!\n";
+      // TODO: fix this
+      $outgoing = array();
     }
-    $this->markSeenProduction($production, $factorIndex);
+    else {
+      foreach ($production->getTerms() as $term) {
+        $termOutgoing = $this->linkTerm($term, $incoming);
 
-    foreach ($production->getTerms() as $term) {
-      $outgoingNodes = array_merge($outgoingNodes, $this->linkTerm($term, $incomingNodes));
+        $outgoing = array_merge($outgoing, $termOutgoing);
+      }
     }
-    return $outgoingNodes;
+
+    return $outgoing;
   }
 
-  /*
-   * Link individual rule alternatives for a production
-   */
-  private function linkTerm($term, $incomingNodes=array()) {
-    print"Linking term " . $term->getID() . "\n";
-    print "----------------------------------\n";
-    $pda = $this->pda;
+  private function linkTerm($term, $incoming) {
+    if (!is_array($incoming)) $incoming = array($incoming);
 
-    $currentNode = null;
-    $outgoingNodes = $incomingNodes;
-    $factorIndex = 0;
-    foreach ($term->getFactors() as $factor) {
-      print "On factor " . $factor->getID() . " at index $factorIndex\n";
-      if (!$factor->isTerminal()) { // factor is a production reference
-        print "@@@ REFERENCE: " . $factor->getID() . " WITH INCOMING " . $this->PRINT_ARRAY($outgoingNodes) . "\n";
-        $outgoingNodes = $this->linkProduction($factor, $outgoingNodes, $factorIndex);
-        print "Got outgoing nodes " . $this->PRINT_ARRAY($outgoingNodes) . " from production ref " . $factor->getID() . "\n";
-        $incomingNodes = array();
+    $outgoing = $incoming;
+    $factors = $term->getFactors();
+    $factorsCount = count($factors);
+    foreach ($factors as $index => $factor) {
+      if (!$factor->isTerminal()) {
+        $recursingOn = ($index == $factorsCount-1) ? $term : null;
+        $outgoing = $this->linkProduction($factor, $outgoing, $recursingOn);
       }
       else {
-        // print "On factor " . $factor->getID() . "\n";
-        $factorID = $factor->getID();
-        $factorNode = $pda->createNode($factorID);
-
-        while ($incomingNode = array_pop($incomingNodes)) {
-          print "!!! INCOMING Adding transition from $incomingNode on $factorID to $factorNode\n";
-          $pda->addTransition($incomingNode, $factorID, $factorNode);
-          // $incomingNode->addTransition($factorID, $factorNode); // TODO
-        }
-
-        if ($currentNode) {
-          print "!!! Adding transition from $currentNode on $factorID to $factorNode\n";
-          $pda->addTransition($currentNode, $factorID, $factorNode);
-          // $currentNode->addTransition($factorID, $factorNode); // TODO - replace above once PDA refactor is in place
-        }
-        $currentNode = $factorNode;
-
-        $outgoingNodes = array($currentNode);
+        $outgoing = $this->linkFactor($factor, $outgoing);
       }
-      $factorIndex++;
     }
 
-    print "Returning from term " . $term->getID() . " with outgoing nodes " . $this->PRINT_ARRAY($outgoingNodes) . "\n";
-    return $outgoingNodes;
+    return $outgoing;
   }
 
+  private function linkFactor($factor, $incoming) {
+    if (!is_array($incoming)) $incoming = array($incoming);
 
-  private function haveSeenProduction($production, $index) {
-    return array_key_exists($production->getID(), $this->linkedProductions) && $this->linkedProductions[$production->getID()] === $index;
-  }
+    $pda = $this->pda;
+    $factorID = $factor->getID();
+    $factorNode = $pda->createNode($factorID);
+    while ($incomingNode = array_pop($incoming)) {
+      print "Adding FACTOR transition from $incomingNode on $factorID to $factorNode\n";
+      $pda->addTransition($incomingNode, $factorID, $factorNode);
+      // $incomingNode->addTransition($factorID, $factorNode); // TODO - change once new PDA is implemented
+    }
 
-  private function markSeenProduction($production, $index) {
-    $this->linkedProductions[$production->getID()] = $index;
-  }
-
-  private function PRINT_ARRAY($array) {
-    if (!$array) return;
-    return array_reduce($array, function($a, $b) { return "$a $b"; }, "");
+    return array($factorNode);
   }
 }
 ?>
